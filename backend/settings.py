20f29240e65b1aed0f187a56926e43bad49c83ae
FR13ND-ly <57838113+FR13ND-ly@users.noreply.github.com>
2022-07-03 19:58:53 +0300

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -13,11 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
-import environ
-
-env = environ.Env()
-environ.Env.read_env()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = 'django-insecure-mxs40h*7ab9q7tgu_6ix(3@6e$wq)dk7fak-j7@=2nuz=y3=)w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
